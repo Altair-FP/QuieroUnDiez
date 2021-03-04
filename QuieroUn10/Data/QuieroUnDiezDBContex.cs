78f2b8964858b128532638d35b016cefb80a46b7
alejandrogemelo1 <alejandrogemelo1@gmail.com>
2021-03-04 21:35:45 +0100

    ProyectoInicial

---
+++ QuieroUn10/Data/QuieroUnDiezDBContex.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QuieroUn10.Data
+{
+    public class QuieroUnDiezDBContex : DbContext
+    {
+        public QuieroUnDiezDBContex()
+        {
+
+        }
+
+        public QuieroUnDiezDBContex(DbContextOptions<QuieroUnDiezDBContex> options)
+           : base(options)
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            IConfiguration configuration = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .Build();
+            optionsBuilder.UseSqlServer(configuration.GetConnectionString("DefaultConnection"));
+        }
+
+    }
+}
---
+++ QuieroUn10/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace QuieroUn10.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
